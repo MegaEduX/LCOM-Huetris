898e4f4b3d56f6c7da8dbcf644e0cc0632004ccc
Eduardo Almeida <ei12018@fe.up.pt>
2014-01-03 20:31:30 +0000

    Missing only the final doc!

--- extra/install.sh
+++ extra/install.sh
@@ -20,4 +20,10 @@ cp ../conf/huetris /etc/system.conf.d/huetris
 cp ../extra/start-huetris /usr/bin/start-huetris
 chmod +x /usr/bin/start-huetris
 
+echo "Cleaning Up..."
+
+rm .depend
+rm *.o
+rm *.d
+
 echo "Done! You can now start the game with # start-huetris. Have fun!"
